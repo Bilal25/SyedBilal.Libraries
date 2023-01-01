4d2de8c849400ad9d3aa060b0d6b12aa13f22827
bilalali181 <syebilal.ezhire@gmail.com>
2023-01-02 03:12:34 +0500

    Calanderview data

---
+++ app/src/main/java/com/syedbilalali/syedbilallibraries/CalanderAct.kt
@@ -0,0 +1,13 @@
+package com.syedbilalali.syedbilallibraries
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class CalanderAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_calander)
+    }
+}
