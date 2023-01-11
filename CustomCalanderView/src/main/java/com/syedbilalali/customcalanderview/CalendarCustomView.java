3e5ac4b23ec0d28003e98cb1c1be758ebd3c8a86
bilalali181 <syebilal.ezhire@gmail.com>
2023-01-11 20:11:29 +0500

    with out view pager

--- CustomCalanderView/src/main/java/com/syedbilalali/customcalanderview/CalendarCustomView.java
+++ CustomCalanderView/src/main/java/com/syedbilalali/customcalanderview/CalendarCustomView.java
@@ -1,10 +1,13 @@
 package com.syedbilalali.customcalanderview;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -12,6 +15,7 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -34,6 +38,11 @@ public class CalendarCustomView extends LinearLayout {
     private GridView calendarGridView;
     public static String dateset;
     private Button addEventButton;
+    private GestureDetector mDetector;
+    private LinearLayout linearLayout;
+
+
+
     public static boolean selectDate = false;
     public static ArrayList<EventObjects> allEvents=new ArrayList<>();
     //testcommit
@@ -53,6 +62,18 @@ public class CalendarCustomView extends LinearLayout {
     private GridAdapter mAdapter;
     private boolean dateType = false;
     private String firstDate,seconDate;
+    private long CLICK_DURATION = 400; // TODO: your timeout here
+
+
+    private float x1;
+    private float y1;
+    private float t1;
+
+    private float x2;
+    private float y2;
+    private float t2;
+    private GestureDetector gestureDetector;
+
 
     public CalendarCustomView(Context context) {
         super(context);
@@ -84,6 +105,7 @@ public class CalendarCustomView extends LinearLayout {
         super(context, attrs, defStyleAttr);
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void initializeUILayout(){
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.calendar_layout, this);
@@ -92,8 +114,20 @@ public class CalendarCustomView extends LinearLayout {
         currentDate = (TextView)view.findViewById(R.id.display_current_date);
         addEventButton = (Button)view.findViewById(R.id.add_calendar_event);
         calendarGridView = (GridView)view.findViewById(R.id.calendar_grid);
+        gestureDetector = new GestureDetector(context, new SwipeGestureDetector());
+        View.OnTouchListener gestureListener = new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }};
+        calendarGridView.setOnTouchListener(gestureListener);
     }
-
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gestureDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
     private void setPreviousButtonClickEvent(){
         previousButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -173,17 +207,6 @@ public class CalendarCustomView extends LinearLayout {
         Log.d(TAG, "Number of date " + dayValueInCells.size());
         String sDate = formatter.format(cal.getTime());
         currentDate.setText(sDate);
-
-
-//        if(callservices){
-//             SimpleDateFormat formatterdate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-//             String dates = formatterdate.format(cal.getTime());
-//             callservices = false;
-//             allEvents.clear();
-////              if(CalanderFragment.not_detail != null){
-////                CalanderFragment.not_detail.callservices(dates);
-////            }
-//        }
         dateset = String.valueOf(sDate);
         mAdapter = new GridAdapter(context, dayValueInCells, cal, allEvents);
         calendarGridView.setAdapter(mAdapter);
@@ -191,6 +214,7 @@ public class CalendarCustomView extends LinearLayout {
         if(allEvents.size() > 2){
             mAdapter.update(allEvents,firstDate,seconDate,allEvents);
         }
+
     }
 
     private String getDate(String date) {
@@ -264,4 +288,96 @@ public class CalendarCustomView extends LinearLayout {
 
     }
 
+
+    class SimpleGestureListener  extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d("TAG","onDown: ");
+
+            // don't return false here or else none of the other
+            // gestures will work
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Log.i("TAG", "onSingleTapConfirmed: ");
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Log.i("TAG", "onLongPress: ");
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Log.i("TAG", "onDoubleTap: ");
+            return true;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                float distanceX, float distanceY) {
+            Log.i("TAG", "onScroll: ");
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            Log.d("TAG", "onFling: ");
+            return true;
+        }
+    }
+
+
+    private class SwipeGestureDetector
+            extends GestureDetector.SimpleOnGestureListener {
+        // Swipe properties, you can change it to make the swipe
+        // longer or shorter and speed
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 200;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2,
+                               float velocityX, float velocityY) {
+            try {
+                float diffAbs = Math.abs(e1.getY() - e2.getY());
+                float diff = e1.getX() - e2.getX();
+
+                if (diffAbs > SWIPE_MAX_OFF_PATH)
+                    return false;
+
+                // Left swipe
+                if (diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    onLeftSwipe();
+
+                    // Right swipe
+                } else if (-diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    onRightSwipe();
+                }
+            } catch (Exception e) {
+                Log.e("YourActivity", "Error on gestures");
+            }
+            return false;
+        }
+    }
+    private void onLeftSwipe() {
+        Toast.makeText(context, "left!",
+                Toast.LENGTH_LONG).show();
+        cal.add(Calendar.MONTH, 1);
+        setUpCalendarAdapter();
+    }
+
+    private void onRightSwipe() {
+        Toast.makeText(context, "right!",
+                Toast.LENGTH_LONG).show();
+        cal.add(Calendar.MONTH, -1);
+        setUpCalendarAdapter();
+    }
 }
